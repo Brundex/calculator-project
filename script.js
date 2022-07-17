9c322635bb1b86ed6d93224e89a6dd3031b1abe8
Brundex <puodziunasb@gmail,com>
2022-07-17 01:55:23 -0300

    added math operation functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function substract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
