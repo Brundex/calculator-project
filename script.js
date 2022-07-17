64ad7b407b4ef784be1e1ea34e3e408883b6ca81
Brundex <puodziunasb@gmail,com>
2022-07-17 03:40:42 -0300

    added populateDisplay function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operation functions
 function add(num1, num2) {
     return num1 + num2
 }
@@ -17,3 +18,20 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     console.log(operator(num1, num2))
 }
+
+// Populate display
+const displayContent = [];
+
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', populateDisplay);
+});
+
+function populateDisplay() {
+    displayContent.push(this.innerHTML);
+    display.textContent = displayContent.join("");
+}
+
+//
