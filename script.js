285433bb2f388daf6f4727451d9492c5d16a8380
Brundex <puodziunasb@gmail,com>
2022-07-18 20:06:47 -0300

    added math background img

--- script.js
+++ script.js
@@ -30,8 +30,37 @@ buttons.forEach(button => {
 });
 
 function populateDisplay() {
-    displayContent.push(this.innerHTML);
-    display.textContent = displayContent.join("");
+    // If there are no numbers yet, operators should not be used
+    if (this.className === 'button operator' && (displayContent.includes('+') ||
+        displayContent.includes('-') ||
+        displayContent.includes('/') ||
+        displayContent.includes('x'))) {
+        return
+    }
+    // Only will display numbers, or operators after numbers
+    else if (this.className === 'digit button' || displayContent.length > 0) {
+        //save operation that is being used
+        if (this.className === 'button operator') {
+            window.currentOperation = this.id;
+            window.index = displayContent.length;
+        }
+        displayContent.push(this.innerHTML);
+        display.textContent = displayContent.join("");
+        console.log(displayContent);
+    }
 }
 
-//
+// Use operate function when pressing "="
+const equal = document.getElementById('equal');
+
+// function equalBehavior {
+//     if (!displayContent.length) 
+//     const firstFactor = displayContent.slice(0, index); 
+// }
+// const firstFactor = displayContent.slice(0, index);
+// console.log(firstFactor);
+// const secondFactor = 10;
+// Condiciones para que funcione el equal:
+// si no hay nada, display muestra 0
+// si solo hay numero, sin index 
+// equal.addEventListener('click', operate(currentOperation, firstFactor, secondFactor))   
